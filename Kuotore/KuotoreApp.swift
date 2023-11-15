730656676bbca775bc596a494ec9b46bd294870b
kakizaki hayate <hayate.h.0704@gmail.com>
2023-11-15 16:25:41 +0900

    initial project

---
+++ Kuotore/KuotoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  KuotoreApp.swift
+//  Kuotore
+//
+//  Created by 柿崎逸 on 2023/11/15.
+//
+
+import SwiftUI
+
+@main
+struct KuotoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
