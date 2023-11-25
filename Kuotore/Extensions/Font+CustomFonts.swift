1039f6a8678c997329b6440753c2263094403abe
znak99 <xhahekcldhkznak0113@gmail.com>
2023-11-25 17:31:17 +0900

    add: カスタムフォント

---
+++ Kuotore/Extensions/Font+CustomFonts.swift
@@ -0,0 +1,19 @@
+//
+//  Font+CustomFonts.swift
+//  Kuotore
+//
+//  Created by SeungWoo Hong on 2023/11/25.
+//
+
+import SwiftUI
+
+// メイン
+extension Font {
+    static let bizBold = "BIZUDPGothic-Bold"
+    static let bizRegular = "BIZUDPGothic-Regular"
+}
+
+// タイトルなど
+extension Font {
+    static let mochiy = "MochiyPopOne-Regular"
+}
