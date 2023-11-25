fd7259b3f7da1ad6125d6115a740411fc1cafe84
znak99 <xhahekcldhkznak0113@gmail.com>
2023-11-25 21:05:01 +0900

    add: 種目リスト

--- Kuotore/App/Constants.swift
+++ Kuotore/App/Constants.swift
@@ -10,4 +10,5 @@ final class Constants {
     
     static let username = "日本電子"
     static let trainingTimes = [4, 6, 8, 12, 0, 3, 1]
+    static let trainings = ["腕立て伏せ", "スクアット", "プランク"]
 }
--- Kuotore/Views/HomeView.swift
+++ Kuotore/Views/HomeView.swift
@@ -40,31 +40,77 @@ struct HomeView: View {
                         Spacer()
                     }
                     .foregroundStyle(.appSecondary)
-                    GeometryReader(content: { proxy in
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 12)
-                                .foregroundStyle(.appSecondary)
-                                .shadow(color: .black.opacity(0.25), radius: 4, x: 0, y: 2)
-                            HStack {
-                                ForEach(Constants.trainingTimes, id: \.self) { time in
-                                    Spacer()
-                                    VStack {
-                                        RoundedRectangle(cornerRadius: 12)
-                                            .frame(width: 12)
-                                            .frame(height: proxy.size.height / 5)
-                                            .foregroundStyle(.white)
-                                        Text("1日前")
-                                            .font(.custom(Font.bizRegular, size: 8))
-                                    }
-                                    Spacer()
-                                }
+                    HStack(alignment: .bottom) {
+                        ForEach(Constants.trainingTimes, id: \.self) { time in
+                            Spacer()
+                            VStack {
+                                RoundedRectangle(cornerRadius: 12)
+                                    .frame(width: 12, height: (CGFloat(time) + 1) * 4)
+                                    .foregroundStyle(.white)
+                                Text("3日前")
+                                    .font(.custom(Font.bizRegular, size: 8))
+                                    .padding(.top, 2)
                             }
-                            .frame(maxWidth: proxy.size.width / 1.2)
+                            Spacer()
                         }
-                        .frame(maxHeight: proxy.size.height / 3.5)
-                    })
+                    }
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 80)
+                    .padding()
+                    .background {
+                        RoundedRectangle(cornerRadius: 12)
+                            .foregroundStyle(.appSecondary)
+                    }
                 }
                 .padding(.top)
+                
+                // 種目リスト
+                HStack {
+                    Image(systemName: "dumbbell.fill")
+                    Text("今すぐ運動を始める")
+                        .font(.custom(Font.bizRegular, size: 16))
+                    Spacer()
+                }
+                .foregroundStyle(.appSecondary)
+                .padding(.top)
+                
+                ScrollView {
+                    ForEach(Constants.trainings, id: \.self) { training in
+                        NavigationLink(destination: EmptyView()) {
+                            HStack {
+                                Text(training)
+                                    .padding(.leading, 24)
+                                    .foregroundStyle(.black)
+                                Spacer()
+                                Image(systemName: "chevron.right")
+                                    .foregroundStyle(.appSecondary)
+                            }
+                            .frame(maxWidth: .infinity)
+                            .font(.custom(Font.bizRegular, size: 16))
+                            .padding()
+                            .background {
+                                Image("list-item-bg")
+                            }
+                        }
+                    }
+                    
+                    NavigationLink(destination: EmptyView()) {
+                        HStack {
+                            Text("カスタム種目生成")
+                                .padding(.leading, 24)
+                                .foregroundStyle(.black)
+                            Spacer()
+                            Image(systemName: "plus")
+                                .foregroundStyle(.appSecondary)
+                        }
+                        .frame(maxWidth: .infinity)
+                        .font(.custom(Font.bizRegular, size: 16))
+                        .padding()
+                        .background {
+                            Image("list-item-bg")
+                        }
+                    }
+                }
                 
                 Spacer()
             }
