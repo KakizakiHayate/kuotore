841f3d0d721393353decac0bf2175c726f208433
znak99 <xhahekcldhkznak0113@gmail.com>
2023-11-25 19:09:17 +0900

    add: 運動時間記録グラフ

--- Kuotore/App/Constants.swift
+++ Kuotore/App/Constants.swift
@@ -9,4 +9,5 @@ final class Constants {
     private init() {}
     
     static let username = "日本電子"
+    static let trainingTimes = [4, 6, 8, 12, 0, 3, 1]
 }
--- Kuotore/Views/HomeView.swift
+++ Kuotore/Views/HomeView.swift
@@ -11,8 +11,9 @@ struct HomeView: View {
     var body: some View {
         NavigationStack {
             VStack {
+                // ヘッダー
                 HStack {
-                    // Logo
+                    // ロゴ
                     HStack {
                         Image("logo")
                             .resizable()
@@ -30,7 +31,40 @@ struct HomeView: View {
                     EmptyView()
                 }
                 
-                
+                // 運動時間グラフ
+                VStack {
+                    HStack {
+                        Image(systemName: "chart.xyaxis.line")
+                        Text("運動時間記録")
+                            .font(.custom(Font.bizRegular, size: 16))
+                        Spacer()
+                    }
+                    .foregroundStyle(.appSecondary)
+                    GeometryReader(content: { proxy in
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 12)
+                                .foregroundStyle(.appSecondary)
+                                .shadow(color: .black.opacity(0.25), radius: 4, x: 0, y: 2)
+                            HStack {
+                                ForEach(Constants.trainingTimes, id: \.self) { time in
+                                    Spacer()
+                                    VStack {
+                                        RoundedRectangle(cornerRadius: 12)
+                                            .frame(width: 12)
+                                            .frame(height: proxy.size.height / 5)
+                                            .foregroundStyle(.white)
+                                        Text("1日前")
+                                            .font(.custom(Font.bizRegular, size: 8))
+                                    }
+                                    Spacer()
+                                }
+                            }
+                            .frame(maxWidth: proxy.size.width / 1.2)
+                        }
+                        .frame(maxHeight: proxy.size.height / 3.5)
+                    })
+                }
+                .padding(.top)
                 
                 Spacer()
             }
