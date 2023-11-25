5b73e78c60742e29c260135f940b5aeeb8945891
znak99 <xhahekcldhkznak0113@gmail.com>
2023-11-25 17:53:36 +0900

    add: ホームヘッダー作成

---
+++ Kuotore/App/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Kuotore
+//
+//  Created by SeungWoo Hong on 2023/11/25.
+//
+
+final class Constants {
+    private init() {}
+    
+    static let username = "日本電子"
+}
--- Kuotore/Views/HomeView.swift
+++ Kuotore/Views/HomeView.swift
@@ -10,8 +10,32 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         NavigationStack {
-            Text("Hello world")
-                .font(.custom(Font.bizBold, size: 24))
+            VStack {
+                HStack {
+                    // Logo
+                    HStack {
+                        Image("logo")
+                            .resizable()
+                            .frame(width: 48, height: 48)
+                        VStack(alignment: .leading) {
+                            Text("\(Constants.username)の")
+                                .font(.custom(Font.bizBold, size: 12))
+                            Text("クオトレ")
+                                .font(.custom(Font.mochiy, size: 24))
+                        }
+                        .foregroundStyle(.appPrimary)
+                    }
+                    Spacer()
+                    // TODO: - 設定画面作ることになったらここから画面遷移
+                    EmptyView()
+                }
+                
+                
+                
+                Spacer()
+            }
+            .padding()
+            .background(Color.BG)
         }
     }
 }
