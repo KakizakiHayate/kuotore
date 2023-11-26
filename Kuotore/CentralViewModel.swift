21fc01b4ad4c8ccac689ce9d1223240d87c1c64d
kakizaki hayate <hayate.h.0704@gmail.com>
2023-11-26 23:40:09 +0900

    fix: CentralViewModelのコードをリファクタ

--- Kuotore/CentralViewModel.swift
+++ Kuotore/CentralViewModel.swift
@@ -11,8 +11,8 @@ import os
 
 class CentralViewModel: NSObject, ObservableObject {
 
-    @Published var message: String = ""
-    var centralManager: CBCentralManager!
+    @Published var message = ""
+    var centralManager: CBCentralManager?
     var discoveredPeripheral: CBPeripheral?
     var transferCharacteristic: CBCharacteristic?
     var writeIterationsComplete = 0
@@ -23,11 +23,16 @@ class CentralViewModel: NSObject, ObservableObject {
 
     override init() {
         super.init()
-        centralManager = CBCentralManager(delegate: self, queue: nil, options: [CBCentralManagerOptionShowPowerAlertKey: true])
+        centralManager = CBCentralManager(delegate: self,
+                                          queue: nil,
+                                          options: [CBCentralManagerOptionShowPowerAlertKey: true])
     }
+}
 
+// MARK: - CentralViewModel
+extension CentralViewModel {
     func stopAction() {
-        centralManager.stopScan()
+        centralManager?.stopScan()
     }
 
     /// Bluetoothデバイスとの通信が不要になったときに呼ぶ
@@ -37,7 +42,7 @@ class CentralViewModel: NSObject, ObservableObject {
 
         guard let service = discoveredPeripheral.services else { return }
         let characteristics = service.compactMap { $0.characteristics }
-                                     .flatMap { $0 }
+            .flatMap { $0 }
         characteristics.forEach {
             if $0.uuid == TransferService.characteristicUUID && $0.isNotifying {
                 self.discoveredPeripheral?.setNotifyValue(false, for: $0)
@@ -45,7 +50,7 @@ class CentralViewModel: NSObject, ObservableObject {
             }
         }
 
-        centralManager.cancelPeripheralConnection(discoveredPeripheral)
+        centralManager?.cancelPeripheralConnection(discoveredPeripheral)
         os_log("ペリフェラルとの接続を解除しました")
     }
 
@@ -65,21 +70,21 @@ class CentralViewModel: NSObject, ObservableObject {
 
     private func retrievePeripheral() {
 
-        let connectedPeripherals: [CBPeripheral] = (centralManager.retrieveConnectedPeripherals(withServices: [TransferService.serviceUUID]))
+        let connectedPeripherals = (centralManager?.retrieveConnectedPeripherals(withServices: [TransferService.serviceUUID]))
 
-        os_log("Found connected Peripherals with transfer service: %@", connectedPeripherals)
+        os_log("Found connected Peripherals with transfer service: %@", connectedPeripherals ?? "No Peripherals")
 
-        if let connectedPeripheral = connectedPeripherals.last {
+        if let connectedPeripheral = connectedPeripherals?.last {
             os_log("Connecting to peripheral %@", connectedPeripheral)
             self.discoveredPeripheral = connectedPeripheral
-            centralManager.connect(connectedPeripheral, options: nil)
+            centralManager?.connect(connectedPeripheral, options: nil)
         } else {
-            centralManager.scanForPeripherals(withServices: [TransferService.serviceUUID], options: nil)
+            centralManager?.scanForPeripherals(withServices: [TransferService.serviceUUID], options: nil)
         }
     }
 }
 
-
+// MARK: - CentralViewModel: CBCentralManagerDelegate
 extension CentralViewModel: CBCentralManagerDelegate {
 
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
@@ -115,22 +120,23 @@ extension CentralViewModel: CBCentralManagerDelegate {
         }
     }
 
-    // 検索したペリフェラルに接続
-    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
-
-        guard RSSI.intValue >= -50 else {
-            return
-        }
+    /// 検索したペリフェラルに接続
+    func centralManager(_ central: CBCentralManager,
+                        didDiscover peripheral: CBPeripheral,
+                        advertisementData: [String : Any],
+                        rssi RSSI: NSNumber
+    ) {
+        guard RSSI.intValue >= -50 else { return }
 
         if discoveredPeripheral != peripheral {
             discoveredPeripheral = peripheral
-            centralManager.connect(peripheral, options: nil)
+            centralManager?.connect(peripheral, options: nil)
         }
     }
 
-    // サービスを検索
+    /// サービスを検索
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
-        centralManager.stopScan()
+        centralManager?.stopScan()
 
         connectionIterationsComplete += 1
         writeIterationsComplete = 0
@@ -141,13 +147,19 @@ extension CentralViewModel: CBCentralManagerDelegate {
         peripheral.discoverServices([TransferService.serviceUUID])
     }
 
-    // ペリフェラルとの接続に失敗したとき
-    func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
+    /// ペリフェラルとの接続に失敗したとき
+    func centralManager(_ central: CBCentralManager,
+                        didFailToConnect peripheral: CBPeripheral,
+                        error: Error?
+    ) {
         cleanup()
     }
 
-    // ペリフェラルから切断されたとき
-    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+    /// ペリフェラルから切断されたとき
+    func centralManager(_ central: CBCentralManager,
+                        didDisconnectPeripheral peripheral: CBPeripheral,
+                        error: Error?
+    ) {
         discoveredPeripheral = nil
 
         if connectionIterationsComplete < defaultIterations {
@@ -158,27 +170,30 @@ extension CentralViewModel: CBCentralManagerDelegate {
     }
 }
 
-
+// MARK: - CBCentralManagerDelegate: CBPeripheralDelegate
 extension CentralViewModel: CBPeripheralDelegate {
 
-    // Characteristicを検索
+    /// Characteristicを検索
     func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
         if error != nil {
             cleanup()
             return
-        }
+        } else {
+            guard let peripheralServices = peripheral.services else {
+                return
+            }
 
-        guard let peripheralServices = peripheral.services else {
-            return
-        }
-
-        for service in peripheralServices {
-            peripheral.discoverCharacteristics([TransferService.characteristicUUID], for: service)
+            for service in peripheralServices {
+                peripheral.discoverCharacteristics([TransferService.characteristicUUID], for: service)
+            }
         }
     }
 
-    // ペリフェラルがcharacteristicsを見つけたことを知らせる
-    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+    /// ペリフェラルがcharacteristicsを見つけたことを知らせる
+    func peripheral(_ peripheral: CBPeripheral,
+                    didDiscoverCharacteristicsFor service: CBService,
+                    error: Error?
+    ) {
 
         if let error = error {
             print("Error discovering characteristics: \(error.localizedDescription)")
@@ -196,59 +211,64 @@ extension CentralViewModel: CBPeripheralDelegate {
         }
     }
 
-    // ペリフェラルからデータが届いたことを知らせる
-    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
-        if let error = error {
+    /// ペリフェラルからデータが届いたことを知らせる
+    func peripheral(_ peripheral: CBPeripheral,
+                    didUpdateValueFor characteristic: CBCharacteristic,
+                    error: Error?
+    ) {
+        if let error {
             print("Error discovering characteristics: \(error.localizedDescription)")
             cleanup()
             return
-        }
-
-        guard let characteristicData = characteristic.value else { return }
-        guard let json = try? JSONSerialization.jsonObject(with: characteristicData, options: []) as? [String: Int] else {
-            return
-        }
-        print(json["distance"] ?? 0)
-        print(type(of: json["distance"] ?? 0))
-
-        guard let stringFromData = String(data: characteristicData, encoding: .utf8) else {
-            return
-        }
-
-        print("Received \(characteristicData.count) bytes: \(stringFromData)")
-
-        if stringFromData == "EOM" {
-            message = String(data: data, encoding: .utf8) ?? ""
-            writeData()
-
         } else {
-            data.append(characteristicData)
+            guard let characteristicData = characteristic.value else { return }
+            guard let json = try? JSONSerialization.jsonObject(with: characteristicData, options: []) as? [String: Int] else {
+                return
+            }
+            print(json["distance"] ?? 0)
+            print(type(of: json["distance"] ?? 0)) // ここでInt型の距離を表示している
+
+            guard let stringFromData = String(data: characteristicData, encoding: .utf8) else {
+                return
+            }
+
+            print("Received \(characteristicData.count) bytes: \(stringFromData)")
+
+            if stringFromData == "EOM" {
+                message = String(data: data, encoding: .utf8) ?? ""
+                writeData()
+            } else {
+                data.append(characteristicData)
+            }
         }
     }
 
-    // 指定されたcharacteristicの通知の要求をペリフェラルが受信したことを通知
-    func peripheral(_ peripheral: CBPeripheral, didUpdateNotificationStateFor characteristic: CBCharacteristic, error: Error?) {
+    /// 指定されたcharacteristicの通知の要求をペリフェラルが受信したことを通知
+    func peripheral(_ peripheral: CBPeripheral,
+                    didUpdateNotificationStateFor characteristic: CBCharacteristic,
+                    error: Error?
+    ) {
 
-        if let error = error {
+        if let error {
             print("Error changing notification state: \(error.localizedDescription)")
             return
-        }
-
-        guard characteristic.uuid == TransferService.characteristicUUID else {
-            return
-        }
-
-        if characteristic.isNotifying {
-            // 通知開始
-            print("Notification began on \(characteristic)")
         } else {
-            // 通知が停止してるからペリフェラルとの接続を解除
-            print("Notification stopped on \(characteristic). Disconnecting")
-            cleanup()
+            guard characteristic.uuid == TransferService.characteristicUUID else {
+                return
+            }
+
+            if characteristic.isNotifying {
+                // 通知開始
+                print("Notification began on \(characteristic)")
+            } else {
+                // 通知が停止してるからペリフェラルとの接続を解除
+                print("Notification stopped on \(characteristic). Disconnecting")
+                cleanup()
+            }
         }
     }
 
-    // ペリフェラルがcharacteristicのアップデートを送信する準備が整ったことを通知
+    /// ペリフェラルがcharacteristicのアップデートを送信する準備が整ったことを通知
     func peripheralIsReady(toSendWriteWithoutResponse peripheral: CBPeripheral) {
         print("Peripheral is ready, send data")
         writeData()
