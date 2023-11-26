1736c01eb12e0b4a72e98a590a9aa745c1d6ebbf
znak99 <xhahekcldhkznak0113@gmail.com>
2023-11-26 17:13:14 +0900

    add: 運動した情報のモデル作成

--- Kuotore/Models/RealmObjects/Training.swift
+++ Kuotore/Models/RealmObjects/Training.swift
@@ -5,4 +5,15 @@
 //  Created by SeungWoo Hong on 2023/11/26.
 //
 
-import Foundation
+import RealmSwift
+
+/*
+ １回筋トレを行った時のモデル
+ */
+class Training: Object {
+    @Persisted var name: String
+    @Persisted var repeatCount: Int = 0
+    @Persisted var elapsedTime: Int = 0
+    @Persisted var consumedCalorie: Float
+    @Persisted var distances: RealmSwift.List<Int> = RealmSwift.List<Int>()
+}
