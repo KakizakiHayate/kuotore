a892fb2e930d393a19bb24d28f463a663d0787f1
kakizaki hayate <hayate.h.0704@gmail.com>
2023-11-27 00:50:52 +0900

    種目を生成するViewとViewModelを追加

---
+++ Kuotore/ViewModels/AddTrainingEvent/AddTrainingEventViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  AddTrainingEventViewModel.swift
+//  Kuotore
+//
+//  Created by 柿崎逸 on 2023/11/27.
+//
+
+import Foundation
+
+class AddTrainingEventViewModel: ObservableObject {
+    // MARK: - Property Wrappers
+    @Published var name = ""
+}
---
+++ Kuotore/Views/AddTrainingEvent/AddTrainingEvent.swift
@@ -0,0 +1,34 @@
+//
+//  AddTrainingEvent.swift
+//  Kuotore
+//
+//  Created by 柿崎逸 on 2023/11/27.
+//
+
+import SwiftUI
+
+struct AddTrainingEvent: View {
+    // MARK: - Property Wrappers
+    @StateObject private var bluetoothManager = CentralViewModel()
+    @StateObject private var vm = AddTrainingEventViewModel()
+    // MARK: - Body
+    var body: some View {
+        VStack {
+            TextField("種目名を入力", text: $vm.name)
+                .padding()
+
+            // TODO: - Pickerで反復か持続選択させる？
+
+            Button {
+
+            } label: {
+                Text("スタート")
+            }
+        }
+    } // body
+} // view
+
+// MARK: - Preview
+#Preview {
+    AddTrainingEvent()
+}
