59862268837125ad90e553aac0fe870386456c52
kakizaki hayate <hayate.h.0704@gmail.com>
2023-11-30 07:08:01 +0900

    add: Loggerを追加

---
+++ Kuotore/Utils/Logger.swift
@@ -0,0 +1,21 @@
+//
+//  Logger.swift
+//  Kuotore
+//
+//  Created by 柿崎逸 on 2023/11/29.
+//
+
+import Foundation
+import os
+
+public enum Logger {
+    public static let standard: os.Logger = .init(
+        subsystem: Bundle.main.bundleIdentifier!,
+        category: LogCategory.standard.rawValue
+    )
+}
+
+// MARK: - Privates
+private enum LogCategory: String {
+     case standard = "Standard"
+}
