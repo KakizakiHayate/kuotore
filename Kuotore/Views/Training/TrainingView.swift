1fa468857b12a56fb4e0577319c4a83ace407f95
kakizaki hayate <hayate.h.0704@gmail.com>
2023-12-01 12:16:44 +0900

    update:  bluetoothを使ってカウントできるようにした

--- Kuotore/Views/Training/TrainingView.swift
+++ Kuotore/Views/Training/TrainingView.swift
@@ -2,7 +2,7 @@
 //  TrainingView.swift
 //  Kuotore
 //
-//  Created by 柿崎逸 on 2023/11/29.
+//  Created by SeungWoo Hong on 2023/11/29.
 //
 
 import SwiftUI
@@ -11,29 +11,141 @@ struct TrainingView: View {
     // MARK: - Property Wrappers
     @StateObject private var bluetoothManager = CentralViewManager.shared
     @StateObject private var vm = TrainingViewModel()
+    @State var flag = false
+    @State private var countdown: Int = 5
+    // MARK: - Properties
+    let trainingInfo: TrainingInfo
 
     // MARK: - Body
     var body: some View {
-        VStack {
-            Text("最高記録：\(vm.highestCount)")
-            Text("回数: \(vm.trainingCount)")
-                .onChangeInteractivelyAvailable(bluetoothManager.distance) { _, newValue in
-                    Task {
-                        await vm.averageCount(newValue)
+        NavigationStack {
+            GeometryReader { proxy in
+                VStack {
+                    if countdown > 0 {
+                        Spacer()
+                        HStack(alignment: .bottom) {
+                            Spacer()
+                            Text("\(countdown)")
+                                .font(.custom(Font.appBlack, size: proxy.size.width / 4))
+                                .foregroundStyle(.appPrimary)
+                            Text("秒後")
+                                .font(.custom(Font.appBold, size: proxy.size.width / 16))
+                                .offset(y: proxy.size.height / -20)
+                            Spacer()
+                        }
+                        Text("\(trainingInfo.name)を始めます")
+                            .font(.custom(Font.appBold, size: proxy.size.width / 16))
+                        Text("準備してください")
+                            .font(.custom(Font.appBold, size: proxy.size.width / 16))
+                        Spacer()
+                    } else {
+                        HStack {
+                            Spacer()
+                            Image("logo-primary")
+                                .resizable()
+                                .frame(width: proxy.size.width / 12)
+                                .frame(height: proxy.size.width / 12)
+                            Text("\(trainingInfo.name)中...")
+                                .font(.custom(Font.appBold, size: proxy.size.width / 16))
+                            Spacer()
+                        }
+                        HStack {
+                            Text("\(vm.trainingCount)")
+                                .font(.custom(Font.appBlack, size: proxy.size.width / 4))
+                                .foregroundStyle(.black)
+                                .frame(width: proxy.size.width / 3)
+                                .frame(height: proxy.size.width / 3)
+                                .background(Color.appPrimary)
+                                .clipShape(Circle())
+                                .padding(.horizontal)
+                                .minimumScaleFactor(0.0001)
+                                .onChangeInteractivelyAvailable(bluetoothManager.distance) { _, newValue in
+                                    Task {
+                                        await vm.averageCount(newValue)
+                                    }
+                                }
+
+                            Text("回")
+                                .font(.custom(Font.appBlack, size: proxy.size.width / 8))
+                        }
+                        .padding(.top)
+                        HStack {
+                            Text("予想消費カロリー")
+                            Spacer()
+                            Text("46kcal")
+                        }
+                        .font(.custom(Font.appBold, size: proxy.size.width / 20))
+                        .padding(.top)
+                        ScrollView {
+                            Button(action: {}, label: {
+                                Text("やり直す")
+                                    .font(.custom(Font.appBold, size: proxy.size.width / 24))
+                                    .foregroundStyle(.black)
+                                    .frame(maxWidth: .infinity)
+                                    .padding(.vertical, proxy.size.height / 60)
+                                    .background(Color.appPrimary)
+                                    .clipShape(RoundedRectangle(cornerRadius: 12))
+                            })
+                            .padding(.top)
+
+                            Button {
+                                bluetoothManager.stopAction()
+                            } label: {
+                                Text("トレーニングを終わる")
+                                    .font(.custom(Font.appBold, size: proxy.size.width / 24))
+                                    .foregroundStyle(.black)
+                                    .frame(maxWidth: .infinity)
+                                    .padding(.vertical, proxy.size.height / 60)
+                                    .background(Color.appPrimary)
+                                    .clipShape(RoundedRectangle(cornerRadius: 12))
+                            }.padding(.vertical)
+                            
+                            Button {} label: {
+                                Text("ホームに戻る")
+                                    .font(.custom(Font.appBold, size: proxy.size.width / 24))
+                                    .foregroundStyle(.white)
+                                    .frame(maxWidth: .infinity)
+                                    .padding(.vertical, proxy.size.height / 60)
+                                    .background(Color.red)
+                                    .clipShape(RoundedRectangle(cornerRadius: 12))
+                            }.padding(.top)
+                            HStack {
+                                Spacer()
+                                Text("* 記録されません")
+                                    .font(.custom(Font.appBold, size: proxy.size.width / 32))
+                                    .foregroundStyle(.red)
+                            }
+                        }
+                        .padding(.top, proxy.size.height / 6)
+                        Spacer()
                     }
                 }
-            Button {
-                bluetoothManager.stopAction()
-            } label: {
-                Text("運動を終了する")
+                .padding()
             }
-        }.task {
-            await vm.readHighestRecord()
+            .onAppear {
+                if !trainingInfo.isExecuted {
+                    flag.toggle()
+                } else {
+                    let timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+                        if self.countdown > 0 {
+                            self.countdown -= 1
+                        } else {
+                            timer.invalidate()
+                        }
+                    }
+                    RunLoop.main.add(timer, forMode: .common)
+                }
+            }
+            .fullScreenCover(isPresented: $flag, onDismiss: {
+                RealmManager.setExecutedStatus(training: trainingInfo)
+            }, content: {
+                InitializeTrainingView(trainingInfo: trainingInfo)
+            })
+            .toolbar(.hidden)
         }
-    } // body
-} // view
+    }
+}
 
-// MARK: - Preview
 #Preview {
-    TrainingView()
+    TrainingView(trainingInfo: TrainingInfo())
 }
